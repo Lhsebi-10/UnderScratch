7f015a68f0abdde83767bc81e6879414004975db
Lhsebi_10 <lhsebi10@gmail.com>
2025-07-05 11:17:11 +0200

    removed docs folders from dev

--- docs/assets/js/aba21aa0.9b753817.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkunder_scratch_docs=self.webpackChunkunder_scratch_docs||[]).push([[742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
